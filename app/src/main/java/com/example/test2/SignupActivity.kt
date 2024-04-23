b75149c317a6bff43da84bb21fd48533167460df
antoniuv <varona.antoniu@gmail.com>
2024-04-24 00:07:07 +0300

    salut

---
+++ app/src/main/java/com/example/test2/SignupActivity.kt
@@ -0,0 +1,52 @@
+package com.example.test2
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.test2.databinding.ActivitySignupBinding
+
+class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var databaseHelper: DatabaseHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        databaseHelper = DatabaseHelper(this)
+
+        binding.signupButton.setOnClickListener{
+            val signupUsername = binding.signupUsername.text.toString()
+            val signupPassword = binding.signupPassword.text.toString()
+            signupDatabase(signupUsername, signupPassword)
+        }
+
+        binding.loginRedirect.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+    }
+
+    private fun signupDatabase(username: String, password: String){
+        val insertedRowId = databaseHelper.insertUser(username, password)
+        if(insertedRowId != -1L){
+            Toast.makeText(this, "Sign up Successful", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            Toast.makeText(this, "Sign up Failed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+}
